d9681a02f2e1fd9fa46cb6e7fb24bb6c598f9e64
mmiranda96 <a01631246@itesm.mx>
2016-03-14 20:11:43 +0100

    Test commit

---
+++ main.py
@@ -0,0 +1,17 @@
+import time
+import pyupm_grove as grove
+import pyupm_i2clcd as lcd
+
+# Initialize Jhd1313m1 at 0x3E (LCD_ADDRESS) and 0x62 (RGB_ADDRESS) 
+myLcd = lcd.Jhd1313m1(0, 0x3E, 0x62)
+
+myLcd.setCursor(0,0)
+# RGB Blue
+#myLcd.setColor(53, 39, 249)
+
+# RGB Red
+myLcd.setColor(0, 255, 0)
+myLcd.write('Disponible')
+time.sleep(3000)
+myLcd.setColor(255, 0, 0)
+myLcd.write('No disponible')
