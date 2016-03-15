57d8f193069fad9439906328eadbcc5279e3454d
mmiranda96 <a01631246@itesm.mx>
2016-03-15 16:27:43 +0100

    Test commit

--- main.py
+++ main.py
@@ -2,7 +2,16 @@ import time
 import pyupm_grove as grove
 import pyupm_i2clcd as lcd
 import pyupm_ttp223 as touch
+import pyupm_servo as servo
 
-touchI = grove.GroveButton(0)
-touchO = grove.GroveButton(1)
+buttonI = grove.GroveButton(8)
+buttonO = grove.GroveButton(7)
 
+touchO = touch.TTP223(4)
+
+display = lcd.Jhd1313m1(0, 0x3E, 0x62)
+
+bar = servo.ES08A(5)
+
+while True:
+    print button.value()
