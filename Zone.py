5bf7701c6fa29e7d34912c63da2d5c1658aadcc3
mmiranda96 <a01631246@itesm.mx>
2016-03-16 02:00:34 +0100

    Test commit

--- Zone.py
+++ Zone.py
@@ -1,4 +1,5 @@
 import time
+import mosquitto
 import threading
 import pyupm_grove as grove
 import pyupm_i2clcd as lcd
@@ -11,7 +12,9 @@ class Zone:
         self.regSpaces = regSpaces
         self.spSpaces = spSpaces
         self.avSpaces = regSpaces
+        self.display = lcd.Jhd1313m1(0, 0x3E, 0x62)
         self.exits = exits #exits must be an array of tuplets: (port, Zone)
+        
         
     def checkExit(self, port):
         for e in self.exits:
@@ -23,4 +26,19 @@ class Zone:
         #Connects to DB
         self.avSpaces -= 1
         
-    
+    def displayAvailability(self):
+        if self.avSpaces > 0:
+            self.display.setCursor(0, 0)
+            self.display.write("Available places:")
+            if self.avSpaces >= self.regSpaces / 2:
+                self.display.setColor(0, 255, 0)
+            elif self.avSpaces >= self.regSpaces * 3 / 4:
+                self.display.setColor(255, 255, 0)
+            else:
+                self.display.setColor(255, 127, 0)
+            self.display.setCursor(1, 0)
+            self.display.write(self.avSpaces)
+        else:
+            self.display.setCursor(0, 0)
+            self.display.write("No available places")
+            self.display.setColor(255, 0, 0)
--- main.py
+++ main.py
@@ -1,15 +1,10 @@
-import time
-import threading
+import Zone
 import pyupm_grove as grove
 import pyupm_i2clcd as lcd
 import pyupm_ttp223 as touch
 import pyupm_servo as servo
 
-buttonI = grove.GroveButton(8)
-buttonO = grove.GroveButton(7)
+exits = [(4, ttp223.TTP223(4))]
+a = Zone(1, 20, 0, exits)
 
-touchO = touch.TTP223(4)
-
-display = lcd.Jhd1313m1(0, 0x3E, 0x62)
-
-bar = servo.ES08A(5)
+a.displayAvailability()
