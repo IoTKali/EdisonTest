d39ca2ba3f6fda8e4fe763de3f83849b6b65013d
mmiranda96 <a01631246@itesm.mx>
2016-03-16 02:02:31 +0100

    Test commit

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import pyupm_i2clcd as lcd
 import pyupm_ttp223 as touch
 import pyupm_servo as servo
 
-exits = [(4, ttp223.TTP223(4))]
+exits = [(4, touch.TTP223(4))]
 a = Zone(1, 20, 0, exits)
 
 a.displayAvailability()
