cb0afd082a661d9df8ea9b7c2ac625c58681eb7e
mmiranda96 <a01631246@itesm.mx>
2016-03-17 22:32:26 +0100

    Test commit

--- sensors.py
+++ sensors.py
@@ -18,24 +18,3 @@ def doubleTouchPulse(touch, op):
             while True:
                 if checkTouchPulse(touch):
                     return True
-
-def updateDisplay(display, maxSpaces, spaces):
-    display.clear()
-        if spaces > 0:
-            if spaces >= maxSpaces / 2:
-                display.setColor(0, 255, 0)
-            elif spaces >= maxSpaces / 4:
-                display.setColor(255, 255, 0)
-            else:
-                display.setColor(255, 127, 0)
-                
-            display.setCursor(0, 0)
-            display.write("Available places:")
-            display.setCursor(1, 0)
-            display.write(str(spaces))
-        else:
-            display.setColor(255, 0, 0)
-            display.setCursor(0, 0)
-            display.write("No available")
-            display.setCursor(1, 0)
-            display.write("places")
