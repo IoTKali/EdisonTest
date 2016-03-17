93e2c6c19e04aaa7beaaa8c215c7cd61341c9753
mmiranda96 <a01631246@itesm.mx>
2016-03-17 08:24:38 +0100

    Test commit

--- test.py
+++ test.py
@@ -47,9 +47,9 @@ def checkExit(sensor, op):
             while True:
                 if checkPulse(sensor):
                     if op == "add":
-                        avSpaces += 1
+                        avSpaces = avSpaces + 1
                     else:
-                        avSpaces -= 1
+                        avSpaces = avSpaces - 1
                     updateDisplay()
     
 thread.start_new_thread(checkExit, (inputSensor, "add", ))
