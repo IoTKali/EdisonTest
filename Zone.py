d3449879b1ac797e02a98a088e5efe035a7dc889
mmiranda96 <a01631246@itesm.mx>
2016-03-18 01:47:07 +0100

    Test commit

--- Zone.py
+++ Zone.py
@@ -71,6 +71,7 @@ class Zone:
         self.client.on_connect = self.on_connect
         self.client.on_message = self.on_message
         for t in self.threadArr:
+            t.start()
             t.join()
         while True:
             pass
