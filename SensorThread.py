0cd40e9641aafb010a5076ad9ee33293ae93a85c
mmiranda96 <a01631246@itesm.mx>
2016-03-18 05:15:45 +0100

    Test commit

--- SensorThread.py
+++ SensorThread.py
@@ -16,6 +16,7 @@ class SensorThread(threading.Thread):
             self.lock.acquire()
             if sensors.doubleTouchPulse(self.sensor):
                 self.client.publish(self.topic, msg)
+                print self.topic + "-" + msg
             self.Zone.updateDisplay()
             self.lock.release()
         
